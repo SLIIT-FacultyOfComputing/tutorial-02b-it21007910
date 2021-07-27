74c1209a40f838fe7cee8c3a9ec236fbda74b67c
it21007910 <87459065+it21007910@users.noreply.github.com>
2021-07-27 06:32:35 +0000

    ww=hat did you change?

--- tute01.cpp
+++ tute01.cpp
@@ -1,30 +1,117 @@
+exercise 1
+#include<iostream>
+using namespace std;
 
-/*Exercise 1 - Calculations
+int main(){
+  float cm, inches;
 
-Convert the C program given below which converts a length given in cm to inches to a C++ program.
+  cout<<"Enter a length in cm : ";
+  cin>>cm;
 
-Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.
+  inches = cm / 2.54;
 
-e.g.
+  cout<<"Length in inches is "<<inches<<endl;
+  return 0;
+}#include<iostream>
+using namespace std;
 
-float data1;
-int data2;
-scanf("%f", &data1); --> std::cin >> data1;
-scanf("%d", &data2); --> std::cin >> data2; 
-You already know that printf() in C is std::cout in C++ e.g.
+int main(){
+  float cm, inches;
 
-printf("Hello World") --> std::cout << "Hello World";
-2.54cm = 1 inch
-*/
+  cout<<"Enter a length in cm : ";
+  cin>>cm;
 
+  inches = cm / 2.54;
 
-#include <stdio.h>
-int main() 
+  cout<<"Length in inches is "<<inches<<endl;
+  return 0;
+}
+
+//exercise2 
+
+#include<iostream>
+using namespace std;
+
+int main()
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
-    return 0;
-}  
+  double salary, netSalary;
+  int etype, otHrs, otRate; 
+
+  cout<<"Enter Employee Type : ";
+  cin>>etype;
+  cout<<"Enter Salary  : ";
+  cin>>salary;
+  cout<<"Enter OtHrs : ";
+  cin>>otHrs;
+
+  switch(etype){
+    case 1:
+     otRate = 1000;
+     break;
+    case 2:
+     otRate = 1500;
+     break;
+    default:
+     otRate = 1700;
+     break;
+  }
+  netSalary = salary + otHrs* otRate;
+  cout<<"Net Salary is "<<netSalary<<endl;
+
+exersice3
+#include<iostream>
+using namespace std;
+
+int main(){
+  int no;
+  long fac;
+
+  cout<<"Enter a Number : ";
+  cin>>no;
+
+  fac = 1;
+  for (int r=no; r >= 1; r--)
+  {
+    fac = fac * r;
+  }
+
+  cout<<"factorial of entered number is "<<fac<<endl;
+  return 0;
+}
+ 
+exersice 4
+#include<iostream>
+using namespace std;
+
+//function declaration
+long Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+  cout << "Enter a value for n ";
+  cin >> n;
+  cout << "Enter a value for r ";
+  cin >> r;
+
+  cout << "nCr = ";
+  cout << nCr(n,r);
+  cout << endl;
+  return 0;
+}
+long Factorial(int no)
+{
+  long fac=1;
+  int x;
+  for(x=no; x>=1; x--)
+  {
+    fac=fac*x;
+  }
+  return fac;
+}
+long nCr(int n, int r)
+{
+  long answer;
+  answer=Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return answer;
+}
